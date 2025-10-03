ebe6f5099146402e42c5991475e026cd7f5b145c
BASKARAN5185 <baskarbala5185@gmail.com>
2025-10-03 16:59:37 +0530

    feat: add playwright test setup with pytest fixture and login automation

--- conftest.py
+++ conftest.py
@@ -1,1 +1,31 @@
- 
+import pytest
+from playwright.sync_api import sync_playwright ,Page
+
+@pytest.fixture(scope='session')
+def playwright_instance():
+    playwright = sync_playwright().start()
+    yield playwright
+    playwright.stop()
+    
+@pytest.fixture(scope='session')
+def browser(playwright_instance)  :
+    browser=playwright_instance.chromium.launch(headless=False)
+    yield browser
+    browser.close()
+    
+@pytest.fixture(scope='function') 
+def page(browser)->Page:
+    context=browser.new_context()
+    page=context.new_page()
+    yield page
+    context.close()
+    
+@pytest.fixture
+def login(page):
+    page.goto('https://demo.suiteondemand.com/index.php?action=Login&module=Users')
+    page.locator('#user_name').fill('will')
+    page.locator('#username_password').fill('will')
+    page.locator('#bigbutton').click()
+    yield
+
+        
