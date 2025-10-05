f552a1fe0630acd36cd8c5c464da1991e1caa7fe
BASKARAN5185 <baskarbala5185@gmail.com>
2025-10-05 13:50:59 +0530

    feat(home): add new Locators with action methods

--- pages/home_page.py
+++ pages/home_page.py
@@ -9,6 +9,38 @@ class HomePage(BaseClass):
       COLLABORATION='text="COLLABRATION"'
       ALL='text="ALL"'
       HOME='text="HOME"'
+      CREATE='text="Create"'
+      SEARCH_BUTTON='button#searchbutton'
+      SEARCH_TEXTBOX='input#query_string'
+      NOTIFICATION_ICON='li#desktop_notifications'
+      USER_ICON='button#usermenucollapsed'
+
+      def user_icon_visible(self) -> bool:
+            return self.is_visible(self.USER_ICON)
+      
+      def user_icon_click(self):
+            self.click(self.USER_ICON)
+
+      def notification_icon_visible(self) -> bool:
+            return self.is_visible(self.NOTIFICATION_ICON)
+
+      def notification_icon_click(self):
+            self.click(self.NOTIFICATION_ICON)
+
+      def search_button_visible(self) -> bool:
+          return self.is_visible(self.SEARCH_BUTTON)
+      
+      def search_button_click(self):
+          self.click(self.SEARCH_BUTTON)
+      
+      def seaerch_textbox_visible(self) -> bool:
+          return self.is_visible(self.SEARCH_TEXTBOX) 
+
+      def search_textbox_fill(self,text):
+          self.fill(self.SEARCH_TEXTBOX,text)
+      
+      def create_click(self):
+            self.click(self.CREATE)
       
       def home_icon_click(self):
           self.click(self.HOME_ICON)
