6943b7e1522e89c600a1f734ab46df6379f1cafa
BASKARAN5185 <baskarbala5185@gmail.com>
2025-10-09 10:46:46 +0530

    feat(profile): add employee profile section locators

--- pages/profile_page.py
+++ pages/profile_page.py
@@ -12,3 +12,20 @@ class profile_page(BaseClass):
     
     '''Employee Information under locators'''
     EMPLOYEE_STATUS="(//div[contains(text() , 'Active')])[2]"
+    DEPARTMENT="div[field='department']"
+    REPORT_TO="input#reports_to_name"
+    DISPLAY_EMPLOYEE_RECORDS='div[field="show_on_employees"]'
+    WORK_PHONE="input[name='phone_work']"
+    MOBILE="input[name='phone_mobile']"
+    OTHER_PHONE="input[name='phone_other']"
+    PHONE_FAX="input[name='phone_fax']"
+    HOME_PHONE='input[name="phone_home"]'
+    IM_NAME='input#messenger_id'
+    ADDRESS_CITY='input[name="address_city"]'
+    ADDRESS_POSTAL_CODE='input[name="address_postalcode"]'
+    IM_TYPE='select#messenger_type'
+    ADDRESS_STREET="input#address_street"
+    ADDRESS_STATE="input#address_state"
+    ADDRESS_COUNTRY="input#address_country"
+    DESCRIPTION='textarea'
+    
