ec6fab1fca794004a3ea64f7642cbda100c0ffde
BASKARAN5185 <baskarbala5185@gmail.com>
2025-10-13 13:11:53 +0530

    feat(profile): add user profile section test case

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -16,7 +16,59 @@ def go_to_login_with_profile(page):
     yield profile
 
 @pytest.mark.profilepage
-@pytest.mark.sanity
+@pytest.mark.smoke
 def test_profile_tab_visible(go_to_login_with_profile: ProfilePage):
     profile = go_to_login_with_profile
     profile.user_profilesection_visible(), 'Profile page navigation is faield'
+
+@pytest.mark.profilepage
+@pytest.mark.sanity1
+def test_update_user_profile(go_to_login_with_profile: ProfilePage):
+    profile = go_to_login_with_profile
+    profile.first_name_enter('william')
+    profile.last_namee_enter('smith')
+    profile.upload_photo('C:/Users/ELCOT/Downloads/git progile.png')
+    profile.header_save_button_click()
+    assert 'william' in profile.get_attribute(profile.FIRST_NAME,'value'), 'First name not updated'
+    assert 'smith' in profile.get_attribute(profile.LAST_NAME,'value'), 'Last name not updated'
+    assert profile.is_visible(profile.PHOTO_UPLOAD), 'Profile photo not updated'
+    
+@pytest.mark.profilepage
+@pytest.mark.sanity1
+def test_cancel_user_profile_update(go_to_login_with_profile: ProfilePage):
+    profile = go_to_login_with_profile
+    profile.first_name_enter('william')
+    profile.last_namee_enter('smith')
+    profile.upload_photo('C:/Users/ELCOT/Downloads/git progile.png')
+    profile.header_cancel_button_click()
+    assert 'will' in profile.get_attribute(profile.USER_NAME,'value'), 'First name updated'
+    assert 'chris' in profile.get_attribute(profile.LAST_NAME,'value'), 'Last name updated'
+    assert not profile.is_visible(profile.PHOTO_UPLOAD), 'Profile photo updated'    
+    
+@pytest.mark.profilepage
+@pytest.mark.sanity1
+def test_last_name_empty_validation(go_to_login_with_profile:ProfilePage):
+    profile = go_to_login_with_profile
+    profile.first_name_enter('william')
+    profile.last_namee_enter('')
+    profile.header_save_button_click()
+    assert 'william' in profile.get_attribute(profile.USER_NAME,'value'), 'last name empty validation failed'
+
+@pytest.mark.profilepage
+@pytest.ark.sanity1
+def test_user_name_non_editable(go_to_login_with_profile:ProfilePage):
+    profile = go_to_login_with_profile
+    assert profile.get_attribute(profile.USER_NAME,'disabled') is not None, 'User name field is editable'
+    
+@pytest.mark.profilepage
+@pytest.mark.sanity1
+def test_profile_staus_non_editable(go_to_login_with_profile:ProfilePage):
+    profile = go_to_login_with_profile
+    assert profile.get_attribute(profile.STATUS,'disabled') is not None, 'Status field is editable'
+
+@pytest.mark.profilepage
+@pytest.mark.sanity1
+def test_user_type_non_editable(go_to_login_with_profile:ProfilePage):
+    profile = go_to_login_with_profile
+    assert profile.get_attribute(profile.USER_TYPE,'disabled') is not None, 'User type field is editable'
+           
