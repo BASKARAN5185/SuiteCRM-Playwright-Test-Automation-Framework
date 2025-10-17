f36d43c1bd210d3fab7c556567d1d8c5be67adf8
BASKARAN5185 <baskarbala5185@gmail.com>
2025-10-17 22:39:07 +0530

    feat(account): add account creation page all the pending locator

--- pages/account_creation_page.py
+++ pages/account_creation_page.py
@@ -1,5 +1,6 @@
 from pages.base_class import BaseClass
 
+
 class account_creation_page(BaseClass):
    ''' Billing Address Locators '''
    BILLING_STREET = "#billing_address_street"
@@ -17,11 +18,28 @@ class account_creation_page(BaseClass):
    SHIPPING_COPY_CHECKBOX = "#shipping_checkbox"
 
    ''' Account Info Locators '''
-
    ACCOUNT_TYPE = "#account_type"
    INDUSTRY = "#industry"
    ANNUAL_REVENUE = "#annual_revenue"
    EMPLOYEES = "#employees"
    DESCRIPTION = "#description"
 
+   ''' Assigned User Section Locators '''
+   ASSIGNED_USER_NAME_INPUT = "input#assigned_user_name"
+   ASSIGNED_USER_SELECT_BUTTON = "button#btn_assigned_user_name"
+   ASSIGNED_USER_CLEAR_BUTTON = "button#btn_clr_assigned_user_name"
 
+   ''' Parent Account Field Locators '''
+   PARENT_NAME_INPUT = "input#parent_name"
+   PARENT_SELECT_BUTTON = "button#btn_parent_name"
+   PARENT_CLEAR_BUTTON = "button#btn_clr_parent_name"
+
+   ''' Campaign Field Locators '''
+   CAMPAIGN_NAME_INPUT = "input#campaign_name"
+   CAMPAIGN_SELECT_BUTTON = "button#btn_campaign_name"
+   CAMPAIGN_CLEAR_BUTTON = "button#btn_clr_campaign_name"
+
+   ''' Account Creation Buttons '''
+   SAVE_BUTTON = "button#SAVE"
+   CANCEL_BUTTON = "button#CANCEL"
+   EDIT_FORM = "#EditView"
