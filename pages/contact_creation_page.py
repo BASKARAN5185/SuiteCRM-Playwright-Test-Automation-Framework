8547a86a87bccedda49ed921804913cb2d3980a6
BASKARAN5185 <baskarbala5185@gmail.com>
2025-10-28 15:33:59 +0530

    feat(account): add account information and buttons  actions

--- pages/contact_creation_page.py
+++ pages/contact_creation_page.py
@@ -30,3 +30,42 @@ class CreateContactPage(BaseClass):
 
         #  Confirmation
         self.header_text = page.locator('h2.module-title-text')
+        
+         # --- Action Methods using BaseClass ---
+
+    def select_salutation(self, value: str):
+        self.select_option_by_value(self.salutation_dropdown,value)
+
+    def enter_first_name(self, first_name: str):
+        self.fill(self.first_name_input, first_name)
+
+    def enter_last_name(self, last_name: str):
+        self.fill(self.last_name_input, last_name)
+
+    def enter_office_phone(self, phone: str):
+        self.fill(self.office_phone_input, phone)
+
+    def enter_mobile_phone(self, mobile: str):
+        self.fill(self.mobile_phone_input, mobile)
+
+    def enter_job_title(self, title: str):
+        self.fill(self.job_title_input, title)
+
+    def enter_department(self, dept: str):
+        self.fill(self.department_input, dept)
+
+    def enter_account_name(self, account: str):
+        self.fill(self.account_name_input, account)
+
+    def click_select_account(self):
+        self.click(self.select_account_button)
+
+    def enter_email(self, email: str):
+        self.fill(self.email_input, email)
+
+    def click_save(self):
+        self.click(self.save_button)
+
+    def click_cancel(self):
+        self.click(self.cancel_button)
+        
